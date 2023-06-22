c2aecf509fda10be30855c669da6a93fc6e81256
Jenifer <jnf.asm@gmail.com>
2023-06-22 12:57:11 +0100

    ajustes no form

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -4,44 +4,60 @@ import { connect } from 'react-redux';
 import { addExpense } from '../redux/actions';
 
 class WalletForm extends Component {
+  state = {
+    value: '',
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: '',
+  };
+
   async componentDidMount() {
     const { dispatch } = this.props;
     const data = await fetch('https://economia.awesomeapi.com.br/json/all');
     const response = await data.json();
     const array = Object.keys(response).filter((item) => item !== 'USDT');
-    console.log(array);
     dispatch(addExpense(array));
   }
 
   render() {
+    const { value, description, currency, tag, method } = this.state;
     const { currencies } = this.props;
-    console.log(currencies);
     return (
       <div>
         <div>WalletForm</div>
         <form>
-          <input
-            type="number"
-            data-testid="value-input"
-            placeholder="0"
-          />
-          <input
-            type="text"
-            data-testid="description-input"
-            placeholder="Descricao"
-          />
+          <label htmlFor="valor">
+            Valor:
+            <input
+              type="number"
+              name="value"
+              data-testid="value-input"
+              value={ value }
+              placeholder="0"
+              id="valor"
+            />
+          </label>
           <label htmlFor="currency">
-            <select name="currency" id="currency" data-testid="currency-input">
-              {currencies.map((currency) => (
-                <option key={ currency } value={ currency }>{currency}</option>
+            Moeda:
+            <select
+              name="currency"
+              id="currency"
+              data-testid="currency-input"
+              value={ currency }
+            >
+              {currencies.map((coin) => (
+                <option key={ coin } value={ coin }>{coin}</option>
               ))}
             </select>
           </label>
           <label htmlFor="method">
+            Método de Pagamento:
             <select
               name="method"
               id="method"
               data-testid="method-input"
+              value={ method }
             >
               <option value="dinheiro">Dinheiro</option>
               <option value="credito">Cartão de crédito</option>
@@ -49,10 +65,12 @@ class WalletForm extends Component {
             </select>
           </label>
           <label htmlFor="tag">
+            Tag:
             <select
               name="tag"
               id="tag"
               data-testid="tag-input"
+              value={ tag }
             >
               <option value="alimentacao">Alimentação</option>
               <option value="lazer">Lazer</option>
@@ -60,6 +78,15 @@ class WalletForm extends Component {
               <option value="transporte">Transporte</option>
               <option value="saude">Saúde</option>
             </select>
+          </label>
+          <label>
+            Descrição:
+            <input
+              type="text"
+              data-testid="description-input"
+              value={ description }
+              placeholder="Descricao"
+            />
           </label>
         </form>
       </div>
