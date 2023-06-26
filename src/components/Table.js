9035f2742524a6fba9ec21151c7af715b516f9b4
Jenifer <jnf.asm@gmail.com>
2023-06-26 11:38:11 +0100

    req 7, implementa a lógica da tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,79 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  handleClick = (event) => {
+    event.preventDefault();
+    const { expensesClick } = this.props;
+    const { id } = this.state;
+    expensesClick(this.state);
+    this.getState({
+      id: id + 1,
+      value: '',
+      currency: 'USD',
+      method: 'Dinheiro',
+      tag: 'Alimentação',
+      description: '',
+    });
+  };
+
   render() {
+    const { expensesState } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        {expensesState.map((expense) => (
+          <tbody key={ expense.id }>
+            <tr>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                {parseFloat(expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2)}
+              </td>
+              <td>
+                {parseFloat(expense.value * expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button>Editar</button>
+                <button
+                  data-testid="delete-btn"
+                  onClick={ this.handleClick }
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+
+          </tbody>
+        ))}
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expensesState: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expensesState: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,26 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
     return (
       <div>
         <Header />
         <WalletForm />
-        <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </table>
+        <Table />
       </div>
     );
   }
